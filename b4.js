d9c0ca08daabb04f4014ae5c77a4e90acd3f9203
latp1102 <latp1102@gmail.com>
2025-03-03 21:18:18 +0700

    Session19-4

---
+++ b4.js
@@ -0,0 +1,7 @@
+let number1 = prompt("Nhập số");
+
+if (number1 % 3 == 0 &&  number1 % 5 == 0){
+    document.write("chia hết cho 3 và 5");
+} else {
+    document.write("không chia hêt cho 3 và 5");
+}
