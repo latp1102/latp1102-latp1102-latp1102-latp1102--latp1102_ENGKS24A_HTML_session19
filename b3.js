b4c792eacd63191663262d8de06e1e2b2dcb509c
latp1102 <latp1102@gmail.com>
2025-03-03 21:17:55 +0700

    Session18-3

---
+++ b3.js
@@ -0,0 +1,8 @@
+let number1 = prompt("Nhập số tiền");
+let number2 = prompt("Nhập số tiền");
+
+let myMoney = (number1 / 23000);
+let myMoney1 = (number2 * 23000);
+
+document.write(myMoney, " USD" , "<br>" );
+document.write(myMoney1, " VND");
